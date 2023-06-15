c00bec30d6c23a9d56e84010390350bd2d657ef3
Rishabh Singh <vineetsatya36@gmail.com>
2023-06-15 17:45:38 +0530

    change

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
-const URL =
-  process.env.MONGODB_URI ||
-  `mongodb://${username}:${password}@ac-etbvcow-shard-00-00.pi2qqnu.mongodb.net:27017,ac-etbvcow-shard-00-01.pi2qqnu.mongodb.net:27017,ac-etbvcow-shard-00-02.pi2qqnu.mongodb.net:27017/?ssl=true&replicaSet=atlas-q8wi1d-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const URL = process.env.MONGODB_URI || process.env.DATABASE;
 
 connection(URL);
 
